8c9d47f1b3837d435a45e4ef40cf44996484564d
icuxika <icuxika@outlook.com>
2025-08-22 16:14:08 +0800

    添加使用NSIS打包时的配置和脚本

---
+++ BuildInstallerWithWebView2FixedVersionRuntime.ps1
@@ -0,0 +1,12 @@
+if (Test-Path -Path .\nsis\SourceDir\) {
+    Remove-Item -Recurse -Force .\nsis\SourceDir\
+}
+if (Test-Path -Path '.\nsis\Install TaskbarManager.exe') {
+    Remove-Item '.\nsis\Install TaskbarManager.exe'
+}
+
+Expand-Archive -LiteralPath .\TaskbarManager-1.0.0-win64-WebView2FixedVersionRuntime.zip -DestinationPath .\nsis\SourceDir\
+Move-Item .\nsis\SourceDir\TaskbarManager-1.0.0-win64\* .\nsis\SourceDir\
+Remove-Item -Recurse -Force .\nsis\SourceDir\TaskbarManager-1.0.0-win64\
+
+.\nsis\build-strlen.ps1
